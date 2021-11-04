8fa18e4ec173dac484c204dbf9deb41be36b2702
yyamda <67169750+yyamda@users.noreply.github.com>
2021-11-03 20:32:58 -0700

    debugged filter function

--- Week4/filter_function.py
+++ Week4/filter_function.py
@@ -1,6 +1,6 @@
 import requests
 import json
-from datetime import datetime 
+from datetime import datetime
 
 #For now, we are just going to implement the “general search” which means to search by a query string
 # Should be able to search, filter, sort
@@ -14,7 +14,7 @@ from datetime import datetime
 
 # search_string, sort type, gene string,  phase, status
 
-# database = None
+
 with open('test_data.json', 'r') as content:
     data = json.load(content)['data']
 
@@ -26,24 +26,28 @@ def convert_dateString_to_dateTime(date):
     return datetime.fromisoformat(date)
 
 
-# Returns a list of trials that has been sorted
-#TODO: Debug actual request call (requests library is ONLY returning html script of API page instead of json data)
-#TODO: End_date sorting 
-#TODO: Gene filter (Doesnt seem to be gene attributes in json data)
-def filter_data(search_string=None, sort_type='relevance', gene_filter=None, status_filter=None, phase_filter=None, company_blacklist=[]):
-    search_string = search_string.lower() if search_string else None
+# Returns a list of trials that has been sorted and filtered
 
-    filteredStatusData = list(filter(lambda trial: trial['current_trial_status'] == status_filter if status_filter else True, data))
-    # filteredStatusData = filter(lambda trial: trial['gene'] == gene_filter if not gene_filter else True, filteredStatusData)
-    filteredStatusData = list(filter(lambda trial: search_string in trial['official_title'].lower() if search_string else True, filteredStatusData))
-    filteredStatusData = list(filter(lambda trial: trial['phase'] == phase_filter if phase_filter else True, filteredStatusData))
-    
-    filteredStatusData = list(filter(lambda trial: trial['lead_org'].lower() not in company_blacklist , filteredStatusData))
-    
-    if sort_type == 'start_date': 
-        filteredStatusData.sort(key = lambda trial: convert_dateString_to_dateTime(trial['start_date']).strftime('%s'))
-    
+def filter_data(search_string=None, sort_type=None, status_filter=None, phase_filter=None, company_blacklist=[]):
+    # search_string = search_string.lower() if search_string else None
+    api_base_url = 'https://clinicaltrialsapi.cancer.gov/api/v2/trials?'
+    headers= {'x-api-key': 'T0GzvGRulK6fFeFVzjmeo1Est2KbWeW25OXpyszf'}
+    #API doesnt have sorting nor blacklist fields
+    if sort_type == 'nci_id':
+        api_base_url = api_base_url + 'sort=' + sort_type + '&'
+    if search_string:
+        api_base_url = api_base_url + 'keyword=' + search_string + '&'
+    if status_filter: 
+        api_base_url = api_base_url + 'trial_status=' + status_filter + '&'
+    if phase_filter:
+        api_base_url = api_base_url + 'phase=' + phase_filter + '&'
 
 
-    return filteredStatusData
-print([ trial['start_date'] for trial in filter_data(sort_type = 'start_date', search_string='cancer')])
+    r = requests.get(api_base_url, headers= headers).json()['data']
+
+    filteredData = list(filter(lambda trial: trial['lead_org'].lower() not in company_blacklist , r))
+
+    if sort_type == 'start_date':
+        filteredData.sort(key = lambda trial: convert_dateString_to_dateTime(trial['start_date']).strftime('%s'))
+
+    return filteredData
