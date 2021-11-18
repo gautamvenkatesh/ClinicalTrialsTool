edfbf7901410bfe14e56beb746dbd5c977acc572
Andy Lau <andylau@Andys-MacBook-Pro.local>
2021-11-17 17:53:22 -0800

    added mongodb trial pull and created folder only for backend

---
+++ Backend/get_trials_mongodb.py
@@ -0,0 +1,28 @@
+from pymongo import MongoClient
+import pymongo
+from bson.json_util import dumps
+
+def get_database():
+
+  
+    CONNECTION_STRING = "mongodb+srv://upsync:upsync@cluster0.p5teq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+    client = MongoClient(CONNECTION_STRING)
+
+    return client['burning_rock_db_v2']
+
+
+# This is added so that many files can reuse the function get_database()
+def get_trials_from_db(): 
+    
+    # Get the database
+    dbname = get_database()
+
+    trials_collection = dbname['trials']
+
+    #sort by descending for newest trials first
+    sorted_trials = trials_collection.find({}).sort('nct_id', pymongo.DESCENDING)
+
+    return dumps(sorted_trials)
+
+
