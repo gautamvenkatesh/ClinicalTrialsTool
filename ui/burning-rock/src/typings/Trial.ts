a84b78dbe2c36d27fb1c722df40fe1c35be206ff
gautamvenkatesh <vegautam@gmail.com>
2021-11-21 15:28:17 -0800

    Add trail type

--- ui/burning-rock/src/typings/Trial.ts
+++ ui/burning-rock/src/typings/Trial.ts
@@ -3,6 +3,21 @@
  */
 
 export interface Trial {
-    
+    record_verification_data: string;
+    nci_id: number;
+    official_title: string;
+    lead_org: string;
+    current_trial_status: string;
+    start_date: string;
+    completion_date: string;
+    brief_summary: string;
+    detail_description: string;
+    primary_purpose: string;
+    phase: string;
+    nct_id: string;
+    eligibility: object;
+    sites: string[];
+    found_genes: string[];
+    found_strings: string[];
 }
 
