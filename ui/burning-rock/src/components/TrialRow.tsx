b86af7489b3adbc22b77ebb831450b3ebdf3ebae
gautamvenkatesh <vegautam@gmail.com>
2021-11-21 15:32:06 -0800

    TrialRow start

--- ui/burning-rock/src/components/TrialRow.tsx
+++ ui/burning-rock/src/components/TrialRow.tsx
@@ -1,11 +1,22 @@
 /**
  * Display trial table row
  */
+import { Table } from 'semantic-ui-react'
+import { Trial } from '../typings/Trial';
 
- const TrialRow: React.FC = () => {
+interface TrialRowProps {
+    trials: Trial;
+}
+
+ const TrialRow: React.FC<TrialRowProps> = ({trials}) => {
     return (
-        <>
-        </>
+        <Table.Row>
+            {Object.keys(trials).map((value) => {
+                <Table.Cell>
+                    value
+                </Table.Cell>
+            })}
+        </Table.Row>
     );
 }
 
